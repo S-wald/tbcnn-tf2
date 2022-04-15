3d6a15a3b08ec93ee31e17ed6b2a798382f07271
Sebastian Waldhauser <sebastian.waldhauser@gmx.at>
2022-04-15 17:09:55 +0200

    clean up

--- classifier/Tbcnn.py
+++ classifier/Tbcnn.py
@@ -10,8 +10,6 @@ class Tbcnn(tf.keras.Model):
         self.conv_layer = ContinuousBinaryTreeConvLayer(embedding_size, conv_output_size)
         self.pooling_layer = DynamicMaxPoolingLayer()
         self.classifier = tf.keras.layers.Dense(num_classes, activation='softmax')
-        self.node_input = tf.keras.Input(shape=(None, embedding_size), dtype=tf.float32)
-        self.children_input = tf.keras.Input(shape=(None, None), dtype=tf.int32)
 
     def call(self, inputs):
         x = self.conv_layer([inputs[0], inputs[1]])
