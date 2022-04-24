f12996079c7eb19acc42c399754ed1d628dc2249
Sebastian Waldhauser <sebastian.waldhauser@gmx.at>
2022-04-24 15:38:39 +0200

    refactoring

--- classifier/Tbcnn.py
+++ classifier/Tbcnn.py
@@ -5,9 +5,9 @@ from classifier.layers.DynamicMaxPoolingLayer import DynamicMaxPoolingLayer
 
 
 class Tbcnn(tf.keras.Model):
-    def __init__(self, embedding_size, conv_output_size, num_classes):
+    def __init__(self, feature_size, conv_output_size, num_classes):
         super(Tbcnn, self).__init__()
-        self.conv_layer = ContinuousBinaryTreeConvLayer(embedding_size, conv_output_size)
+        self.conv_layer = ContinuousBinaryTreeConvLayer(feature_size, conv_output_size)
         self.pooling_layer = DynamicMaxPoolingLayer()
         self.classifier = tf.keras.layers.Dense(num_classes, activation='softmax')
         self.argmax = None
