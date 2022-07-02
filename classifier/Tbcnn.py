ef9c70badb374b1f9ce1b392cca2a6d5ea470de7
Sebastian Waldhauser <sebastian.waldhauser@gmx.at>
2022-07-02 18:09:47 +0200

    experiment with multiple conv layers; experiment with metrics for overall importance of nodes

--- classifier/Tbcnn.py
+++ classifier/Tbcnn.py
@@ -5,15 +5,17 @@ from classifier.layers.DynamicMaxPoolingLayer import DynamicMaxPoolingLayer
 
 
 class Tbcnn(tf.keras.Model):
-    def __init__(self, feature_size, conv_output_size, num_classes):
+    def __init__(self, feature_size, num_kernels, num_classes):
         super(Tbcnn, self).__init__()
-        self.conv_layer = ContinuousBinaryTreeConvLayer(feature_size, conv_output_size)
+        self.conv_layer = ContinuousBinaryTreeConvLayer(feature_size, num_kernels)
+        self.conv_layer2 = ContinuousBinaryTreeConvLayer(num_kernels, num_kernels)
         self.pooling_layer = DynamicMaxPoolingLayer()
         self.classifier = tf.keras.layers.Dense(num_classes, activation='softmax')
         self.argmax = None
 
     def call(self, inputs):
         x = self.conv_layer([inputs[0], inputs[1]])
+        x = self.conv_layer2([x, inputs[1]])
         self.argmax, x = self.pooling_layer(x)
         return self.classifier(x)
 
