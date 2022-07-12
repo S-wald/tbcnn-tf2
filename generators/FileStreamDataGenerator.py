fix off-by-one error

generators/FileStreamDataGenerator.py
@@ -75,9 +75,9 @@ class FileStreamDataGenerator(tf.keras.utils.Sequence):
                 if node['node'] in self.node_embeddings:
                     nodes.append(self.node_embeddings[node['node']])
                 else:
-                    nodes.append([0.0]*60)
+                    nodes.append([0.0]*80)
             else:
-                nodes.append(self.__onehot(self.node_map[node['node']], len(self.node_map)+1))
+                nodes.append(self.__onehot(self.node_map[node['node']]-1, len(self.node_map)))
 
         return nodes, children, label
 

vectorizer/AstNode2VecFileStreamDataGenerator2.py
@@ -47,7 +47,7 @@ class AstNode2VecFileStreamDataGenerator2(tf.keras.utils.Sequence):
     def __gen_sample(self, sample):
         context = []
         parent_context = 0
-        target = self.__onehot(self.node_map[sample['target']], len(self.node_map)) 
+        target = self.__onehot(self.node_map[sample['target']]-1, len(self.node_map)) 
         for c in sample['context']:
             context.append(self.node_map[c]) # use index for later embedding lookup
         if len(context) == 0:
@@ -64,7 +64,7 @@ class AstNode2VecFileStreamDataGenerator2(tf.keras.utils.Sequence):
         return children
 
     def __onehot(self, i, total):
-        return [1.0 if (j-1) == i else 0.0 for j in range(total)]
+        return [1.0 if j == i else 0.0 for j in range(total)]
 
     def get_node_map(self):
         return self.node_map